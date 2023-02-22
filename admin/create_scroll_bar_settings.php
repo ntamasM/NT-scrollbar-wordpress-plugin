51a9f64d2122e95f83a3e04f7a9e75b58c59ae3b
ntamasM <109794412+ntamasM@users.noreply.github.com>
2023-02-22 14:51:35 +0200

    0.0.3

--- admin/create_scroll_bar_settings.php
+++ admin/create_scroll_bar_settings.php
@@ -14,6 +14,9 @@ class scrollbarCreation{
         add_settings_field('nt_scrollbar_track_color', 'Scrollbar Track', array($this, 'scrollbar_track_color_HTML'), 'scrollbar-settings-page', 'nt_first_section'); // id, title, display cb, page, section
         register_setting('scrollbaroptions', 'nt_scrollbar_track_color'); // option group, option name, sanitize cb
         
+        add_settings_field('nt_scrollbar_track_opacity', 'Scrollbar Track Opacity', array($this, 'nt_scrollbar_track_opacity_HTML'), 'scrollbar-settings-page', 'nt_first_section'); // id, title, display cb, page, section
+        register_setting('scrollbaroptions', 'nt_scrollbar_track_opacity'); // option group, option name, sanitize cb
+        
         add_settings_field('nt_scrollbar_thumb_color', 'Scrollbar Thumb', array($this, 'scrollbar_thumb_color_HTML'), 'scrollbar-settings-page', 'nt_first_section'); // id, title, display cb, page, section
         register_setting('scrollbaroptions', 'nt_scrollbar_thumb_color'); // option group, option name, sanitize cb
         
@@ -31,6 +34,13 @@ class scrollbarCreation{
         ?>
         <label for="nt_scrollbar_track_color">Choose Any Color:</label>
         <input type="color" name="nt_scrollbar_track_color"  id="nt_scrollbar_track_color" value="<?php echo get_option('nt_scrollbar_track_color','#101010') ?>">
+        <?php
+    }
+    
+    function nt_scrollbar_track_opacity_HTML(){
+        ?>
+        <label for="nt_scrollbar_track_opacity">Choose Any Number:</label>
+        <input type="number" name="nt_scrollbar_track_opacity"  id="nt_scrollbar_track_opacity" min="0" step="0.1" max="1" value="<?php echo get_option('nt_scrollbar_track_opacity','1') ?>"><span> (min=0, max=1)</span>
         <?php
     }
     
