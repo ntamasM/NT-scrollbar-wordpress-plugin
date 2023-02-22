38610da1bb90c192463903d5e2bc784ed1019619
ntamasM <109794412+ntamasM@users.noreply.github.com>
2023-02-22 14:52:23 +0200

    0.0.3

--- public/export_scroll_bar_settings.php
+++ public/export_scroll_bar_settings.php
@@ -1,21 +1,45 @@
 <?php
 
+function hex2RGB($hexStr, $returnAsString = false, $seperator = ',') {
+    $hexStr = preg_replace("/[^0-9A-Fa-f]/", '', $hexStr); // Gets a proper hex string
+    $rgbArray = array();
+    if (strlen($hexStr) == 6) { //If a proper hex code, convert using bitwise operation. No overhead... faster
+        $colorVal = hexdec($hexStr);
+        $rgbArray['red'] = 0xFF & ($colorVal >> 0x10);
+        $rgbArray['green'] = 0xFF & ($colorVal >> 0x8);
+        $rgbArray['blue'] = 0xFF & $colorVal;
+    } elseif (strlen($hexStr) == 3) { //if shorthand notation, need some string manipulations
+        $rgbArray['red'] = hexdec(str_repeat(substr($hexStr, 0, 1), 2));
+        $rgbArray['green'] = hexdec(str_repeat(substr($hexStr, 1, 1), 2));
+        $rgbArray['blue'] = hexdec(str_repeat(substr($hexStr, 2, 1), 2));
+    } else {
+        return false; //Invalid hex color code
+    }
+    return $returnAsString ? implode($seperator, $rgbArray) : $rgbArray; // returns the rgb string or the associative array
+}
+
 function scrollbarsettings () {
     
     $nt_scrollbar_thumb_color = get_option( 'nt_scrollbar_thumb_color', 'black' );
     $nt_scrollbar_thumb_hover_color = get_option( 'nt_scrollbar_thumb_hover_color', '#5a5a5a' );
-    $nt_scrollbar_track_color = get_option( 'nt_scrollbar_track_color', '#101010' );
     $nt_scrollbar_width = get_option( 'nt_scrollbar_width', '15' );
     $nt_scroll_border_radius = get_option( 'nt_scroll_border_radius', '5' );
+    $nt_scrollbar_track_background = hex2RGB(get_option( 'nt_scrollbar_track_color', '#101010' ), 'true');
+    $nt_scrollbar_track_opacity=get_option( 'nt_scrollbar_track_opacity', '1' );
     
     return "
+        html,body
+        {
+        	overflow:overlay;
+        }
+    
         /*Start Scrollbar Habit by Ntamas*/
-
+        
         ::-webkit-scrollbar {
           width: ".$nt_scrollbar_width."px;
         }
         ::-webkit-scrollbar-track {
-          background: $nt_scrollbar_track_color;
+          background: rgba($nt_scrollbar_track_background,".$nt_scrollbar_track_opacity.");
         }
         ::-webkit-scrollbar-thumb {
           background: $nt_scrollbar_thumb_color; 
@@ -25,6 +49,10 @@ function scrollbarsettings () {
           background: $nt_scrollbar_thumb_hover_color;
         }
         /* End Scrollbar Habit by Ntamas*/
+        
+        #wpadminbar {
+            padding-right: ".$nt_scrollbar_width."px;
+        }
     ";
 }
 
